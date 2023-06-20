dd31e3cb5673af5c115cb80776d9f10f87f512b2
Christian Knoll <chris@sectorfive.com>
2023-06-19 23:05:13 -0400

    updating to throw error, and formatting

--- users/user.service.js
+++ users/user.service.js
@@ -2,34 +2,44 @@
 const jwt = require('jsonwebtoken');
 
 // users hardcoded for simplicity, store in a db for production applications
-const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+const users = [
+	{
+		id: 1,
+		username: 'test',
+		password: 'test',
+		firstName: 'Test',
+		lastName: 'User',
+	},
+];
 
 module.exports = {
-    authenticate,
-    getAll
+	authenticate,
+	getAll,
 };
 
 async function authenticate({ username, password }) {
-    const user = users.find(u => u.username === username && u.password === password);
+	const user = users.find(
+		(u) => u.username === username && u.password === password
+	);
 
-    if (!user) throw 'Username or password is incorrect';
+	if (!user) throw new Error('Username or password is incorrect');
 
-    // create a jwt token that is valid for 7 days
-    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+	// create a jwt token that is valid for 7 days
+	const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
 
-    return {
-        ...omitPassword(user),
-        token
-    };
+	return {
+		...omitPassword(user),
+		token,
+	};
 }
 
 async function getAll() {
-    return users.map(u => omitPassword(u));
+	return users.map((u) => omitPassword(u));
 }
 
 // helper functions
 
 function omitPassword(user) {
-    const { password, ...userWithoutPassword } = user;
-    return userWithoutPassword;
+	const { password, ...userWithoutPassword } = user;
+	return userWithoutPassword;
 }
